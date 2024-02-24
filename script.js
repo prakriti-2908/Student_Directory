5bbecac7d84f3dec98173699b1237f941b2d0cd9
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-24 23:47:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -99,6 +99,8 @@ function displayGenderTables(femaleStudents, maleStudents) {
 }
 
 function generateGenderTable(students, title) {
+  const tableBody = document.querySelector('#studentTable tbody');
+  tableBody.innerHTML = '';
   let tableHTML = `<h2 style="margin-left: 2.5vw;">${title}</h2>`;
   tableHTML += `<table  border="1">
                   <thead>
